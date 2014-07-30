c165597ccf08b142a366054431b24138b55d6aa7
Robin Chowdhury <robinch88@gmail.com>
2014-07-30 20:57:51 +0200

    Klar med uppgift1 - Kaprekar

--- Uppgift1.py
+++ Uppgift1.py
@@ -1,16 +1,41 @@
 # Programmeringsteknik webbkurs KTH inlämningsuppgift 1.
 # Robin Chowdhury
-# 2014-07-07
+# 2014-07-30
+# Programmet baseras på Kaprekars upptäckt.
 # Programmet tar in ett 4-siffrigt tal (input).
-# Den tar differansen mellan det 4-siffriga talet och dess invers.
-# Om inte differansen är 6174 så gör den om detta med differansen.
-# Detta görs om tills differansen är 6174. Då returneas hur många
-# gånger som detta behövdes göra
+# Den retunerar antalet gånger som Kaprerars process
+# behövs göras tills den når 6174
 
-# def kaprar(n):
-	 
+# indata: 4-siffrigt tal
+# utdata: antal gånger kaprekars process görs
+# tills talet blir 6174
+def kaprekar(n):
+	i = 0;
+	while True:
+		i += 1
 
+		# castar till str då beräkningen av differansen
+		# gör n till int istället för string
+		n = str(n)
+		large = "".join(sorted(n, reverse=True))
+		small = "".join(sorted(n))
+
+		# Lägger till 0:or så att talet förblir
+		# 4-siffrigt 
+		for x in range(len(n),4):
+			large = large + "0"
+			small = "0" + small
+
+		# castar till int så att man kan beräkna
+		# differansen
+		n = int(large) - int(small)
+		if(n == 6174):
+			return i
+
+
+# En öändlig loop så att man slipper starta om programmet
+# inför varje nytt tal
 while True:
-	s = input("Skriv in ett fyrsiffrig tal")
-	if s == q:
-		break
+	n = input("Skriv in ett fyrsiffrig tal: ")
+	print ("Number of iterations: ", kaprekar(n))
+	
