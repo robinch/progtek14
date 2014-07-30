1075c0ae4427bf2f11e6c6d229fce01c9bca3ea4
Robin Chowdhury <robinch88@gmail.com>
2014-07-30 19:27:04 +0200

    File addded

---
+++ Uppgift1.py
@@ -0,0 +1,16 @@
+# Programmeringsteknik webbkurs KTH inlämningsuppgift 1.
+# Robin Chowdhury
+# 2014-07-07
+# Programmet tar in ett 4-siffrigt tal (input).
+# Den tar differansen mellan det 4-siffriga talet och dess invers.
+# Om inte differansen är 6174 så gör den om detta med differansen.
+# Detta görs om tills differansen är 6174. Då returneas hur många
+# gånger som detta behövdes göra
+
+# def kaprar(n):
+	 
+
+while True:
+	s = input("Skriv in ett fyrsiffrig tal")
+	if s == q:
+		break
