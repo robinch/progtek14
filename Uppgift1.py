432d4ac4cf56aaf016b15cc27546c1c39e455612
Robin Chowdhury <robinch88@gmail.com>
2014-08-12 14:10:38 +0200

    Fixed a thing in uppgift1

--- Uppgift1.py
+++ Uppgift1.py
@@ -7,30 +7,38 @@
 # behövs göras tills den når 6174
 
 # indata: 4-siffrigt tal
-# utdata: antal gånger kaprekars process görs
+# utdata: antal gånger Kaprekars process görs
 # tills talet blir 6174
 def kaprekar(n):
 	i = 0;
+	if(n=="6174"):
+		return i;
 	while True:
-		i += 1
-
 		# castar till str då beräkningen av differansen
-		# gör n till int istället för string
 		n = str(n)
+		
+		if(n=="6174"):
+			return i;
+
+		# i - antalet gånger Kaprerakrs process görs
+		i += 1
+		
+		# gör n till int istället för string
+		
+		# sorterar talet n från största siffra till minsta
 		large = "".join(sorted(n, reverse=True))
+		# sorterar talet n från minsta siffra till största
 		small = "".join(sorted(n))
 
-		# Lägger till 0:or så att talet förblir
-		# 4-siffrigt 
+		# Lägger till 0:or om det behövs så att talet 
+		# förblir 4-siffrigt 
 		for x in range(len(n),4):
 			large = large + "0"
 			small = "0" + small
 
-		# castar till int så att man kan beräkna
-		# differansen
+		# castar till int så att man kan beräkna differansen
 		n = int(large) - int(small)
-		if(n == 6174):
-			return i
+		
 
 
 # En öändlig loop så att man slipper starta om programmet
